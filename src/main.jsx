66eb649a77e3b58b62536ee0c70b5f69fae9a781
sparsharya1699 <YourNewEmail@example.com>
2025-01-14 21:52:18 +0530

    Header-Component Integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react' // Strict Mode for Displaying the Errors with Future Compatible Versions 
-import { createRoot } from 'react-dom/client' //Rendering App Element using Create Root
-import './index.css' // Main Element CSS
-import App from './pages/App.jsx' // Importing the Main Element to Render 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import { StrictMode } from "react"; // Strict Mode for Displaying the Errors with Future Compatible Versions
+import { createRoot } from "react-dom/client"; //Rendering App Element using Create Root
+import "./index.css"; // Main Element CSS
+import App from "./App.jsx"; // Importing the Main Element to Render
+import { BrowserRouter } from "react-router-dom"; //To get react router support in the app component
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+import Header from '../../components/Header/Header'
+const home = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default home
